fd901dbe59406df8e109553a4e9f4dead12c406e
Allen Ray <allentray125@gmail.com>
2021-02-03 23:17:08 -0500

    Adding multi-channel support for player

--- src/ws-audio-api.js
+++ src/ws-audio-api.js
@@ -7,34 +7,46 @@
 //    Frame Duration: 2.5, 5, 10, 20, 40, 60
 //    Buffer Size = sample rate/6000 * 1024
 
-(function(global) {
+(function (global) {
 	var defaultConfig = {
 		codec: {
 			sampleRate: 24000,
 			channels: 1,
 			app: 2048,
 			frameDuration: 20,
-			bufferSize: 4096
+			bufferSize: 4096,
+			calcBuffer: false
 		},
 		server: {
 			host: window.location.hostname
+		},
+		contextOpts: {
+			sampleRate: 24000
 		}
 	};
 
-	var audioContext = new(window.AudioContext || window.webkitAudioContext)();
+	var AudioContext = window.AudioContext || window.webkitAudioContext;
+	var audioContext = false;
 
 	var WSAudioAPI = global.WSAudioAPI = {
-		Player: function(config, socket) {
+		Player: function (config, socket) {
 			this.config = config || {};
 			this.config.codec = this.config.codec || defaultConfig.codec;
 			this.config.server = this.config.server || defaultConfig.server;
-			this.sampler = new Resampler(this.config.codec.sampleRate, audioContext.sampleRate, 1, this.config.codec.bufferSize);
+			this.config.contextOpts = this.config.contextOpts || defaultConfig.contextOpts;
+			audioContext = new AudioContext(this.config.contextOpts);
+
+			if (this.config.codec.calcBuffer) {
+				this.config.codec.bufferSize = ((this.config.codec.sampleRate) / 6000) * 1024;
+			}
+
+			this.sampler = new Resampler(this.config.codec.sampleRate, audioContext.sampleRate, this.config.codec.channels, this.config.codec.bufferSize);
 			this.parentSocket = socket;
 
 			this.decoder = new OpusDecoder(this.config.codec.sampleRate, this.config.codec.channels);
 			this.silence = new Float32Array(this.config.codec.bufferSize);
 		},
-		Streamer: function(config, socket) {
+		Streamer: function (config, socket) {
 			navigator.getUserMedia = (navigator.getUserMedia ||
 				navigator.webkitGetUserMedia ||
 				navigator.mozGetUserMedia ||
@@ -43,19 +55,21 @@
 			this.config = config || {};
 			this.config.codec = this.config.codec || defaultConfig.codec;
 			this.config.server = this.config.server || defaultConfig.server;
-			this.sampler = new Resampler(audioContext.sampleRate, this.config.codec.sampleRate, 1, this.config.codec.bufferSize);
+			this.config.contextOpts = this.config.contextOpts || defaultConfig.contextOpts;
+			audioContext = new AudioContext(this.config.contextOpts);
+			this.sampler = new Resampler(audioContext.sampleRate, this.config.codec.sampleRate, this.config.codec.channels, this.config.codec.bufferSize);
 			this.parentSocket = socket;
 			this.encoder = new OpusEncoder(this.config.codec.sampleRate, this.config.codec.channels, this.config.codec.app, this.config.codec.frameDuration);
 			var _this = this;
 
-			this._makeStream = function(onError) {
-				navigator.getUserMedia({ audio: true }, function(stream) {
+			this._makeStream = function (onError) {
+				navigator.getUserMedia({ audio: true }, function (stream) {
 					_this.stream = stream;
 					_this.audioInput = audioContext.createMediaStreamSource(stream);
 					_this.gainNode = audioContext.createGain();
-					_this.recorder = audioContext.createScriptProcessor(_this.config.codec.bufferSize, 1, 1);
+					_this.recorder = audioContext.createScriptProcessor(_this.config.codec.bufferSize, this.config.codec.channels, this.config.codec.channels);
 
-					_this.recorder.onaudioprocess = function(e) {
+					_this.recorder.onaudioprocess = function (e) {
 						var resampled = _this.sampler.resampler(e.inputBuffer.getChannelData(0));
 						var packets = _this.encoder.encode_float(resampled);
 						for (var i = 0; i < packets.length; i++) {
@@ -71,7 +85,7 @@
 		}
 	};
 
-	WSAudioAPI.Streamer.prototype.start = function(onError) {
+	WSAudioAPI.Streamer.prototype.start = function (onError) {
 		var _this = this;
 
 		if (!this.parentSocket) {
@@ -87,7 +101,7 @@
 		} else if (this.socket.readyState == WebSocket.CONNECTING) {
 			var _onopen = this.socket.onopen;
 
-			this.socket.onopen = function() {
+			this.socket.onopen = function () {
 				if (_onopen) {
 					_onopen();
 				}
@@ -99,7 +113,7 @@
 
 		var _onclose = this.socket.onclose;
 
-		this.socket.onclose = function(event) {
+		this.socket.onclose = function (event) {
 			if (_onclose) {
 				_onclose(event);
 			}
@@ -120,23 +134,23 @@
 		};
 	};
 
-	WSAudioAPI.Streamer.prototype.mute = function() {
+	WSAudioAPI.Streamer.prototype.mute = function () {
 		this.gainNode.gain.value = 0;
 		console.log('Mic muted');
 	};
 
-	WSAudioAPI.Streamer.prototype.unMute = function() {
+	WSAudioAPI.Streamer.prototype.unMute = function () {
 		this.gainNode.gain.value = 1;
 		console.log('Mic unmuted');
 	};
 
-	WSAudioAPI.Streamer.prototype.onError = function(e) {
+	WSAudioAPI.Streamer.prototype.onError = function (e) {
 		var error = new Error(e.name);
 		error.name = 'NavigatorUserMediaError';
 		throw error;
 	};
 
-	WSAudioAPI.Streamer.prototype.stop = function() {
+	WSAudioAPI.Streamer.prototype.stop = function () {
 		if (this.audioInput) {
 			this.audioInput.disconnect();
 			this.audioInput = null;
@@ -156,13 +170,13 @@
 		}
 	};
 
-	WSAudioAPI.Player.prototype.start = function() {
+	WSAudioAPI.Player.prototype.start = function () {
 		var _this = this;
 
 		this.audioQueue = {
 			buffer: new Float32Array(0),
 
-			write: function(newAudio) {
+			write: function (newAudio) {
 				var currentQLength = this.buffer.length;
 				newAudio = _this.sampler.resampler(newAudio);
 				var newBuffer = new Float32Array(currentQLength + newAudio.length);
@@ -171,23 +185,35 @@
 				this.buffer = newBuffer;
 			},
 
-			read: function(nSamples) {
+			read: function (nSamples) {
 				var samplesToPlay = this.buffer.subarray(0, nSamples);
 				this.buffer = this.buffer.subarray(nSamples, this.buffer.length);
 				return samplesToPlay;
 			},
 
-			length: function() {
+			length: function () {
 				return this.buffer.length;
 			}
 		};
 
-		this.scriptNode = audioContext.createScriptProcessor(this.config.codec.bufferSize, 1, 1);
-		this.scriptNode.onaudioprocess = function(e) {
+		this.scriptNode = audioContext.createScriptProcessor(this.config.codec.bufferSize, this.config.codec.channels, this.config.codec.channels);
+		this.scriptNode.onaudioprocess = function (e) {
+			var chans = _this.config.codec.channels;
 			if (_this.audioQueue.length()) {
-				e.outputBuffer.getChannelData(0).set(_this.audioQueue.read(_this.config.codec.bufferSize));
+				var buf = _this.audioQueue.read(_this.config.codec.bufferSize * chans);
+				var channels = [];
+				for (let c = 0; c < chans; c++) {
+					channels.push(e.outputBuffer.getChannelData(c))
+				}
+				for (let i = 0; i < buf.length; i += chans) {
+					for (let c = 0; c < channels.length; c++) {
+						channels[c][i / 2] = buf[i + c];
+					}
+				}
 			} else {
-				e.outputBuffer.getChannelData(0).set(_this.silence);
+				for (let i = 0; i < chans; i++) {
+					e.outputBuffer.getChannelData(i).set(_this.silence);
+				}
 			}
 		};
 		this.gainNode = audioContext.createGain();
@@ -195,53 +221,44 @@
 		this.gainNode.connect(audioContext.destination);
 
 		if (!this.parentSocket) {
-			this.socket = new WebSocket(this.config.server.host);
+			this.socket = new WebSocket(this.config.server);
 		} else {
 			this.socket = this.parentSocket;
 		}
-        //this.socket.onopen = function () {
-        //    console.log('Connected to server ' + _this.config.server.host + ' as listener');
-        //};
-        var _onmessage = this.parentOnmessage = this.socket.onmessage;
-        this.socket.onmessage = function(message) {
-        	if (_onmessage) {
-        		_onmessage(message);
-        	}
-        	if (message.data instanceof Blob) {
-        		var reader = new FileReader();
-        		reader.onload = function() {
-        			_this.audioQueue.write(_this.decoder.decode_float(reader.result));
-        		};
-        		reader.readAsArrayBuffer(message.data);
-        	}
-        };
-        //this.socket.onclose = function () {
-        //    console.log('Connection to server closed');
-        //};
-        //this.socket.onerror = function (err) {
-        //    console.log('Getting audio data error:', err);
-        //};
-      };
+		var _onmessage = this.parentOnmessage = this.socket.onmessage;
+		this.socket.onmessage = function (message) {
+			if (_onmessage) {
+				_onmessage(message);
+			}
+			if (message.data instanceof Blob) {
+				var reader = new FileReader();
+				reader.onload = function () {
+					_this.audioQueue.write(_this.decoder.decode_float(reader.result));
+				};
+				reader.readAsArrayBuffer(message.data);
+			}
+		};
+	};
 
-      WSAudioAPI.Player.prototype.getVolume = function() {
-      	return this.gainNode ? this.gainNode.gain.value : 'Stream not started yet';
-      };
+	WSAudioAPI.Player.prototype.getVolume = function () {
+		return this.gainNode ? this.gainNode.gain.value : 'Stream not started yet';
+	};
 
-      WSAudioAPI.Player.prototype.setVolume = function(value) {
-      	if (this.gainNode) this.gainNode.gain.value = value;
-      };
+	WSAudioAPI.Player.prototype.setVolume = function (value) {
+		if (this.gainNode) this.gainNode.gain.value = value;
+	};
 
-      WSAudioAPI.Player.prototype.stop = function() {
-      	this.audioQueue = null;
-      	this.scriptNode.disconnect();
-      	this.scriptNode = null;
-      	this.gainNode.disconnect();
-      	this.gainNode = null;
+	WSAudioAPI.Player.prototype.stop = function () {
+		this.audioQueue = null;
+		this.scriptNode.disconnect();
+		this.scriptNode = null;
+		this.gainNode.disconnect();
+		this.gainNode = null;
 
-      	if (!this.parentSocket) {
-      		this.socket.close();
-      	} else {
-      		this.socket.onmessage = this.parentOnmessage;
-      	}
-      };
-    })(window);
+		if (!this.parentSocket) {
+			this.socket.close();
+		} else {
+			this.socket.onmessage = this.parentOnmessage;
+		}
+	};
+})(window);
